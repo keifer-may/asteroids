9f207855d53665bef8bbca1dcf8f09b4f8ca48bb
Keifer May <73715008+keifer-may@users.noreply.github.com>
2024-11-11 19:56:50 -0500

    Add groups and add comments to main

--- main.py
+++ main.py
@@ -3,23 +3,44 @@ from constants import *
 from player import Player
 
 def main():
+    ## Init pygame
     pygame.init()
+
+    ## Create groups for our game objects
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    ## All player objects must be updateable & drawable
+    Player.containers = (updateable, drawable)
+
+    ## Create time clock and dt
     timer = pygame.time.Clock()
     dt = 0
+    
+    ## Create screen & player object
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
     while True:
+        ## FPS calcs
         time = timer.tick(60)
         dt = time/1000
 
+        ## Ensures closing window ends game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        ## Update all updateable objects (players only for now)
+        for each in updateable:
+            each.update(dt)
+
+        ## Fill screen black
         screen.fill("black")
-        player.draw(screen)
+
+        ## Draw all drawable objects (players only for now)
+        for each in drawable:
+            each.draw(screen)
 
         pygame.display.flip()
 
