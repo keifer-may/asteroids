be726a524a3fa9303f74646c19590616d825256b
Keifer May <73715008+keifer-may@users.noreply.github.com>
2024-11-11 19:18:34 -0500

    Draw and render player

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     timer = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x=SCREEN_WIDTH/2, y=SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return       
         screen.fill("black")
+        player.draw(screen)
+
         pygame.display.flip()
         time = timer.tick(60)
         dt = time/1000
