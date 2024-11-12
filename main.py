5f5b4fdc0f6d9c8f36a70cfc94c2284749b411cb
Keifer May <73715008+keifer-may@users.noreply.github.com>
2024-11-11 19:43:06 -0500

    Add movement method to player

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ def main():
 
         pygame.display.flip()
 
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+ ##   print("Starting asteroids!")
+ ##   print(f"Screen width: {SCREEN_WIDTH}")
+ ##   print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
